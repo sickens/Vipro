f8f3193a8d155f7667e001d944504b8ed9a46492
Nafis <nafispiyal@gmail.com>
2023-04-20 12:04:48 -0500

    Added volumetric formula

--- Vipro.ino
+++ Vipro.ino
@@ -2,28 +2,29 @@ const int stepPin = 3;
 const int dirPin = 4;
 const int stepPin2 = 5;
 const int dirPin2 = 6;
-
 const int stepsPerRevolution = 12500;
-const int del = 50;
+int del;
 
+//CHANGE THIS VALUE(mL/sec) AND MAKE SURE TO UPLOAD CODE BEFORE RUNNING
+const float mLPerSecond = 0.04;
 
 void setup()
 {
-  // Declare pins as Outputs
+  Serial.begin(9600);
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
   pinMode(stepPin2, OUTPUT);
   pinMode(dirPin2, OUTPUT);
+
+  del = (1/( mLPerSecond / .05 * stepsPerRevolution )) * 1000000/2;
 }
 
 
 void loop()
 {
-  // Set motor direction clockwise
   digitalWrite(dirPin, HIGH);
   digitalWrite(dirPin2, HIGH);
 
-  
   for(int x = 0; x < stepsPerRevolution; x++)
   {
     digitalWrite(stepPin, HIGH);
